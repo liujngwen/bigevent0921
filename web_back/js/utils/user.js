cf576e4925e128b862a3de09a27a4572362cd5d5
liujngwen <177491766@qq.com>
2019-09-21 17:37:11 +0800

    第一天提交完毕

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,26 +1,17 @@
 var user = {
-  /**
-   * 管理员登陆
-   * @param {*} userName 用户名
-   * @param {*} password 密码
-   * @param {*} callBack 回调函数
-   */
   login: function(userName, password, callBack) {
-    // alert(userName + password);
-    $.post(URLIST.user_login, { user_name: userName, password: password }, function(res) {
-      // res 是本次请求，从服务器回来的数据
+    console.log(userName, password);
+    $.post(urlList.user_login,{user_name: userName,password:password}, function(res) {
       callBack(res);
     });
   },
-
   logout: function(callBack) {
-    $.post(URLIST.user_logout, function(res) {
+    $.post(urlList.user_logout,function(res) {
       callBack(res);
     });
   },
-
   getInfo: function(callBack) {
-    $.get(URLIST.user_info, function(res) {
+    $.get(urlList.user_info, function(res) {
       callBack(res);
     });
   }
